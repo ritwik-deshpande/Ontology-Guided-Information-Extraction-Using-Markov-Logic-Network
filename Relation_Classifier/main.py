5807ef203c9d2d9ade0fdd72a0f3c7b68d64a0c0
shalakapatankar <37779508+shalakapatankar@users.noreply.github.com>
2020-04-09 23:11:00 +0530

    Update main.py

--- Relation_Classifier/main.py
+++ Relation_Classifier/main.py
@@ -1,25 +1,19 @@
 import keras
-# from tensorflow.python.keras.models import KerasTextClassifier
-from models import KerasTextClassifier
-import numpy as np
+from Parsers import getDocuments
+from Converters import getGoldTruthTest,getGoldTruthTrain,getIndexes,getRelations
+from KerasTextClassifier import KerasTextClassifier
 from sklearn.model_selection import train_test_split
-import os
-#print(os.getcwd())
-import matplotlib.pyplot as plt
-plt.style.use("ggplot")
 from sklearn.metrics import f1_score, classification_report, accuracy_score
 import numpy as np
 import pandas as pd
-from keras.preprocessing.text import Tokenizer
 import csv
 %matplotlib inline
-from keras.preprocessing.sequence import pad_sequences
 
 if __name__ == '__main__':
          
          documents_train = getDocuments('train.txt')
          relations_train = getRelations(documents_train)
-         gold_truths_train,tr_tr,ts_tr = getGoldTruth(relations_train,documents_train)
+         gold_truths_train,tr_tr,ts_tr = getGoldTruthTrain(relations_train,documents_train)
 	n_relations = len(set(tr_tr))
          
          if sys.argv[1] == 'train':
@@ -33,12 +27,14 @@ if __name__ == '__main__':
 	if sys.argv[1] == 'test':
       
 		kclf = KerasTextClassifier(input_length=50, n_classes=n_relations, max_words=15000)
-                  kclf.load()
-                  label_to_use = list(kclf.encoder.classes_)
-                  pointer = 0;
-                  data = pd.read_csv('base_classifier_test.csv')
-                  print(label_to_use)
-                  with open('relation_classifier_test.csv', 'w', newline='') as file:
+                kclf.load()
+                label_to_use = list(kclf.encoder.classes_)
+                pointer = 0;
+                data = pd.read_csv('base_classifier_test.csv')
+                #print(label_to_use)
+		actual_tags=[]
+		predicted_tags=[]
+                with open('relation_classifier_test.csv', 'w', newline='') as file:
                            writer = csv.writer(file)
                            writer.writerow(["SentenceID","token1","token2","Sentence","Predicted","Gold Truth"])
                            for sentenceID in range(0,288):
@@ -57,3 +53,11 @@ if __name__ == '__main__':
                                                                        gold_truth_indexes[i][1],
                                                       input_sentences[i],label_to_use[y_test_pred[i]],
                                                                        getGoldTruthTest(sentenceID,gold_truth_indexes[i])])
+							predicted_tags.append(label_to_use[y_test_pred[i]])
+							actual_tags.append(getGoldTruthTest(sentenceID,gold_truth_indexes[i]))
+							
+		print('classification_report: \n',classification_report(actual_tags,predicted_tags,digits=3))
+		print('confusion_matrix :\n',confusion_matrix(actual_tags,predicted_tags))						
+					
+				
+			
